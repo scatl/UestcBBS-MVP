3d62bf9a4d29933f0ea20ef59179580bdb9e945e
sca_tl <sca_tl@foxmail.com>
2022-12-20 21:54:49 +0800

    new_post_detail_5

--- buildSrc/src/main/kotlin/Build.kt
+++ buildSrc/src/main/kotlin/Build.kt
@@ -3,8 +3,8 @@
  */
 object BuildVersion {
     const val applicationId     =       "com.scatl.uestcbbs"
-    const val versionCode       =       305
-    const val versionName       =       "3.0.2-beta1"
+    const val versionCode       =       306
+    const val versionName       =       "3.0.2-beta2"
     const val buildToolsVersion =       "32.0.0"
     const val compileSdk        =       32
     const val minSdk            =       23
