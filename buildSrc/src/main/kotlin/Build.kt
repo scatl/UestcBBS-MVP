c1c060b49f8a94a63f12f9ee4c021c905fba2d37
sca_tl <sca_tl@foxmail.com>
2023-04-01 18:37:11 +0800

    发布3.1.0版本

--- buildSrc/src/main/kotlin/Build.kt
+++ buildSrc/src/main/kotlin/Build.kt
@@ -3,8 +3,8 @@
  */
 object BuildVersion {
     const val applicationId     =       "com.scatl.uestcbbs"
-    const val versionCode       =       313
-    const val versionName       =       "3.0.6"
+    const val versionCode       =       314
+    const val versionName       =       "3.1.0"
     const val buildToolsVersion =       "32.0.0"
     const val compileSdk        =       32
     const val minSdk            =       23
